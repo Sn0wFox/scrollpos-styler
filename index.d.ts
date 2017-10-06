047ef3912943c5086aaf6c2afb7ff5219752d3ff
Sn0wFox <ruben.pericas@laposte.net>
2017-10-06 15:42:30 +0200

    Migrate original code

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * ========================================================================
+ * ScrollPos-Styler v0.7.0
+ * https://github.com/acch/scrollpos-styler
+ * ========================================================================
+ * Copyright 2015 Achim Christ
+ * Licensed under MIT (https://github.com/acch/scrollpos-styler/blob/master/LICENSE)
+ * ======================================================================== */
+declare namespace ScrollPosStyler {
+    function init(options: any): void;
+}
+export = ScrollPosStyler;
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
+// ======================== ORIGINAL LICENCE NOTICE =======================
 /**
  * @license
- *  ========================================================================
+ * ========================================================================
  * ScrollPos-Styler v0.7.0
  * https://github.com/acch/scrollpos-styler
  * ========================================================================
@@ -8,44 +9,41 @@
  * Licensed under MIT (https://github.com/acch/scrollpos-styler/blob/master/LICENSE)
  * ======================================================================== */
 
-// JSHint directives
-/* exported ScrollPosStyler */
-
-var ScrollPosStyler = (function(document, window) {
-  "use strict";
-
+namespace ScrollPosStyler {
+  
   /* ====================
    * private variables
    * ==================== */
-  var scrollPosY = 0,
-      busy = false,
+  
+  let scrollPosY = 0;
+  let busy = false;
 
-      // toggle style / class when scrolling below this position (in px)
-      scrollOffsetY = 1,
-
-      // class used to apply scrollPosStyler to
-      spsClass = "sps",
-
-      // choose elements to apply style / class to
-      elements = document.getElementsByClassName(spsClass),
-
-      // style / class to apply to elements when above scroll position
-      classAbove = "sps--abv",
-
-      // style / class to apply to elements when below scroll position
-      classBelow = "sps--blw",
-
-      // tag to set custom scroll offset per element
-      offsetTag = "data-sps-offset";
+  // toggle style / class when scrolling below this position (in px)
+  let scrollOffsetY = 1;
+  
+  // class used to apply scrollPosStyler to
+  let spsClass = "sps";
+  
+  // choose elements to apply style / class to
+  let elements = document.getElementsByClassName(spsClass);
+  
+  // style / class to apply to elements when above scroll position
+  let classAbove = "sps--abv";
+  
+  // style / class to apply to elements when below scroll position
+  let classBelow = "sps--blw";
+  
+  // tag to set custom scroll offset per element
+  let offsetTag = "data-sps-offset";
 
   /* ====================
-   * private funcion to check scroll position
+   * private function to check scroll position
    * ==================== */
   function onScroll() {
     // ensure that events don't stack
     if (!busy) {
       // find elements to update
-      var elementsToUpdate = getElementsToUpdate();
+      let elementsToUpdate = getElementsToUpdate();
 
       if (elementsToUpdate.length > 0) {
         // suspend accepting scroll events
@@ -60,24 +58,24 @@ var ScrollPosStyler = (function(document, window) {
   }
 
   /* ====================
-   * private funcion to find elements to update
+   * private function to find elements to update
    * ==================== */
   function getElementsToUpdate() {
     // get current scroll position from window
     scrollPosY = window.pageYOffset;
 
-    var elementsToUpdate = [];
+    let elementsToUpdate = [];
 
     // iterate over elements
-    // for (var elem of elements) {
-    for (var i = 0; elements[i]; ++i) { // chrome workaround
-      var element = elements[i];
+    // for (let elem of elements) {
+    for (let i = 0; elements[i]; ++i) { // chrome workaround
+      let element = elements[i];
 
       // get offset from element, default to global option
-      var elScrollOffsetY = element.getAttribute(offsetTag) || scrollOffsetY;
+      let elScrollOffsetY = element.getAttribute(offsetTag) || scrollOffsetY;
 
       // check current state of element
-      var elOnTop = element.classList.contains(classAbove);
+      let elOnTop = element.classList.contains(classAbove);
 
       // if we were above, and are now below scroll position...
       if (elOnTop && scrollPosY > elScrollOffsetY) {
@@ -88,7 +86,7 @@ var ScrollPosStyler = (function(document, window) {
           removeClass: classAbove
         });
 
-      // if we were below, and are now above scroll position...
+        // if we were below, and are now above scroll position...
       } else if (!elOnTop && scrollPosY <= elScrollOffsetY) {
         // remember element
         elementsToUpdate.push({
@@ -103,13 +101,13 @@ var ScrollPosStyler = (function(document, window) {
   }
 
   /* ====================
-   * private funcion to update elements
+   * private function to update elements
    * ==================== */
   function updateElements(elementsToUpdate) {
     // iterate over elements
-    // for (var elem of elements) {
-    for (var i = 0; elementsToUpdate[i]; ++i) { // chrome workaround
-      var map = elementsToUpdate[i];
+    // for (let elem of elements) {
+    for (let i = 0; elementsToUpdate[i]; ++i) { // chrome workaround
+      let map = elementsToUpdate[i];
 
       // add style / class to element
       map.element.classList.add(map.addClass);
@@ -131,50 +129,47 @@ var ScrollPosStyler = (function(document, window) {
    *    offsetTag (String): HTML tag used on the element to specify a scrollOffsetY other than the default.
    *
    * ==================== */
-  var pub = {
-    init: function(options) {
-      // suspend accepting scroll events
-      busy = true;
+  export function init(options) {
+    // suspend accepting scroll events
+    busy = true;
 
-      // merge options object with global options
-      if (options) {
-        if (options.spsClass) {
-          spsClass = options.spsClass;
-          elements = document.getElementsByClassName(spsClass);
-        }
-        scrollOffsetY = options.scrollOffsetY || scrollOffsetY;
-        classAbove = options.classAbove || classAbove;
-        classBelow = options.classBelow || classBelow;
-        offsetTag = options.offsetTag || offsetTag;
+    // merge options object with global options
+    if (options) {
+      if (options.spsClass) {
+        spsClass = options.spsClass;
+        elements = document.getElementsByClassName(spsClass);
       }
-
-      // find elements to update
-      var elementsToUpdate = getElementsToUpdate();
-
-      if (elementsToUpdate.length > 0) {
-        // asynchronuously update elements
-        window.requestAnimationFrame(function() {
-          updateElements(elementsToUpdate);
-        });
-      } else {
-        // resume accepting scroll events
-        busy = false;
-      }
+      scrollOffsetY = options.scrollOffsetY || scrollOffsetY;
+      classAbove = options.classAbove || classAbove;
+      classBelow = options.classBelow || classBelow;
+      offsetTag = options.offsetTag || offsetTag;
     }
-  };
 
+    // find elements to update
+    let elementsToUpdate = getElementsToUpdate();
+
+    if (elementsToUpdate.length > 0) {
+      // asynchronuously update elements
+      window.requestAnimationFrame(function() {
+        updateElements(elementsToUpdate);
+      });
+    } else {
+      // resume accepting scroll events
+      busy = false;
+    }
+  }
 
   /* ====================
    * main initialization
    * ==================== */
   // add initial style / class to elements when DOM is ready
-  document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", () => {
     // defer initialization to allow browser to restore scroll position
-    window.setTimeout(pub.init, 1);
+    window.setTimeout(init, 1);
   });
 
   // register for window scroll events
   window.addEventListener("scroll", onScroll);
+}
 
-  return pub;
-})(document, window);
+export = ScrollPosStyler;
